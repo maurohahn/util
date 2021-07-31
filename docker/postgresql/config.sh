38a1eb6a224a26b5beb6577dad4909ea31775a46
Mauro Hahn <mauro@wswork.com.br>
2021-07-30 22:24:19 -0300

    fix sql server and fix postgresql

--- docker/postgresql/config.sh
+++ docker/postgresql/config.sh
@@ -1,5 +1,5 @@
-sudo docker exec -it local_pg psql -U postgres -c "create extension if not exists 'uuid-ossp';"
+sudo docker exec -it local_pg psql -U postgres -c "create extension if not exists \"uuid-ossp\";"
 sudo docker exec -it local_pg psql -U postgres -c "drop database if exists my_database;"
 sudo docker exec -it local_pg psql -U postgres -c "create database my_database encoding='UTF8';"
-sudo docker exec -it local_pg psql -U postgres -c "create role my_user with encrypted password 'my_secret';"
+sudo docker exec -it local_pg psql -U postgres -c "create role my_user with encrypted password 'my_secret' SUPERUSER LOGIN;"
 sudo docker exec -it local_pg psql -U postgres -c "grant all privileges on database my_database to my_user;"
